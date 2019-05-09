77f1340d45b5fa490e3d6319a55fa8cef29c2566
Picasso <prvasquez@ucdavis.edu>
2019-05-09 21:55:59 +0000

    more updated

--- server.R
+++ server.R
@@ -29,13 +29,27 @@ colnames(worm_plant) <- c("query_id",
                           "se",
                           "E",
                           "Score")
+plant_worm <- plant_worm %>% 
+  arrange(query_id, E) %>% 
+  group_by(query_id) %>% 
+  mutate(nextE = lead(E)) %>%
+  ungroup()
+#head(plant_worm,12)
+
+worm_plant <- worm_plant %>% 
+  arrange(query_id, E) %>% 
+  group_by(query_id) %>% 
+  mutate(nextE = lead(E)) %>%
+  ungroup()
+#head(worm_plant,12)
 plant_worm_best <- plant_worm %>%
   arrange(query_id, E, desc(Score)) %>%
-  filter(!duplicated(query_id))
+  filter(!duplicated(query_id)) %>%
+  select(query_id, subject_id, pct_ident, len, E, nextE, Score)
 worm_plant_best <- worm_plant %>%
   arrange(query_id, E, desc(Score)) %>%
-  filter(!duplicated(query_id))
-
+  filter(!duplicated(query_id)) %>%
+  select(query_id, subject_id, pct_ident, len, E, nextE, Score)
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
   
@@ -49,16 +63,16 @@ shinyServer(function(input, output) {
   output$boxPlot <- renderPlot({
     
     # set up the plot
-    pl <- ggplot(data = worm_plant_best,
+    pl_worm_plant <- ggplot(data = worm_plant_best,
                  #Use aes_string below so that input$trait is interpreted
                  #correctly.  The other variables need to be quoted
-                 aes_string(x="pct_ident" > 50,
-                            y=input$trait,
-                            fill="Species"
+                 aes(x=len,
+                    y=input$trait,
+                    color = pct_ident
                  )
     )
     
     # draw the boxplot for the specified trait
-    pl + geom_boxplot()
+    pl_worm_plant + geom_point()
   })
 })
